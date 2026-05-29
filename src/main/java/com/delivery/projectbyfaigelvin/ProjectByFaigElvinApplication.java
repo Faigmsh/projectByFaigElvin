45ad5d785bf49bb70e7d240fd7f019612dbeaf4d
Faig <faig@example.com>
2026-05-29 20:56:08 +0400

    first-c

---
+++ src/main/java/com/delivery/projectbyfaigelvin/ProjectByFaigElvinApplication.java
@@ -0,0 +1,14 @@
+package com.delivery.projectbyfaigelvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectByFaigElvinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectByFaigElvinApplication.class, args);
+        System.out.println("first-run");
+    }
+
+}
---
+++ src/test/java/com/delivery/projectbyfaigelvin/ProjectByFaigElvinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.projectbyfaigelvin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectByFaigElvinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
